1f8449bf16a71d6f22d14dea21645b6add542b9c
linhey <is.linhey@outlook.com>
2023-06-14 17:38:08 +0800

    chore: 移除 url 的特殊判断

--- Source/SwiftyJSON/SwiftyJSON.swift
+++ Source/SwiftyJSON/SwiftyJSON.swift
@@ -866,25 +866,8 @@ extension JSON {
 
     //Optional URL
     public var url: URL? {
-        get {
-            switch type {
-            case .string:
-                // Check for existing percent escapes first to prevent double-escaping of % character
-                if rawString.range(of: "%[0-9A-Fa-f]{2}", options: .regularExpression, range: nil, locale: nil) != nil {
-                    return Foundation.URL(string: rawString)
-                } else if let encodedString_ = rawString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) {
-                    // We have to use `Foundation.URL` otherwise it conflicts with the variable name.
-                    return Foundation.URL(string: encodedString_)
-                } else {
-                    return nil
-                }
-            default:
-                return nil
-            }
-        }
-        set {
-            object = newValue?.absoluteString ?? NSNull()
-        }
+        get { return Foundation.URL(string: rawString) }
+        set { object = newValue?.absoluteString ?? NSNull() }
     }
 }
 
